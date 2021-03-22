bb3c8fbb7dfdabe8e0d853aeaa5436d619179e1f
Pr0214 <44285766+Pr0214@users.noreply.github.com>
2021-03-22 08:30:27 +0800

    Update traceNatives.py

--- traceNatives.py
+++ traceNatives.py
@@ -9,7 +9,6 @@ import idautils
 import idc
 
 
-
 # 获取SO文件名和路径
 def getSoPathAndName():
     fullpath = ida_nalt.get_input_file_path()
@@ -51,29 +50,25 @@ class traceNatives(plugin_t):
         for func in idautils.Functions(ea, ed):
             try:
                 functionName = str(idaapi.ida_funcs.get_func_name(func))
-                demangled = idc.demangle_name(functionName,
-                                              idc.get_inf_attr(
-                                                  idc.INF_SHORT_DN))
-                functionName = demangled if demangled else functionName
                 if len(list(idautils.FuncItems(func))) > 10:
                     # 如果是thumb模式，地址+1
                     arm_or_thumb = idc.get_sreg(func, "T")
                     if arm_or_thumb:
                         func += 1
-                    search_result.append([hex(func), functionName])
+                    search_result.append(hex(func))
             except:
                 pass
 
         so_path, so_name = getSoPathAndName()
-        search_result = [f"{so_name}!{a}!{b}" for a, b in search_result]
-        search_result = "\n".join(search_result)
+        search_result = [f"-a '{so_name}!{offset}'" for offset in search_result]
+        search_result = " ".join(search_result)
 
         save_path = os.path.join(so_path, so_name.split(".")[0] + ".txt")
         with open(save_path, "w", encoding="utf-8")as F:
             F.write(search_result)
 
         print("使用方法如下：")
-        print(f"frida-trace -UF -z {save_path}")
+        print(f"frida-trace -UF -O {save_path}")
 
     def term(self):
         pass
